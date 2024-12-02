08f01ec39f36fe9d4e9519ec04a5a176e996581a
Ragunath Jawahar <ragunath@legacycode.com>
2024-12-02 14:20:11 +0530

    feat!!: remove vercel/analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import localFont from "next/font/local";
 import "./globals.css";
 import {SpeedInsights} from "@vercel/speed-insights/next"
 import React from "react";
-import {Analytics} from "@vercel/analytics/react";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -43,7 +42,6 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} antialiased`}
     >
     <SpeedInsights/>
-    <Analytics/>
     {children}
     </body>
     </html>
